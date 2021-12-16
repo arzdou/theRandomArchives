599e26cde206c4e3da2f610d669f70724bf9d8e2
Jaime <jaimetravesedo125@gmail.com>
2021-12-16 18:59:36 +0100

    Created main for selecting random phrases

---
+++ main.py
@@ -0,0 +1,21 @@
+'''
+For now the main file just creates the text of the tweet
+'''
+
+import json
+from random import randint, choice
+
+DIRECTORY = './_jsons'
+
+episode_number = randint(1, 200)
+file_name = '{}.json'.format(episode_number)
+file_name = (8-len(file_name))*'0' + file_name   # This ensures that the leading 0s are always correct
+
+with open('{}/{}'.format(DIRECTORY, file_name)) as f:
+    statement = json.load(f)
+    
+speaker = choice(list(statement.keys()))
+phrase = choice(statement[speaker])
+text = '{}: {}'.format(speaker, phrase)
+
+print(text)
